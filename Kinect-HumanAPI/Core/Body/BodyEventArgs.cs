fc17c6cfe9740ec652ea83edac0c8b4003646a04
Sebastian Kotstein <sebastian.kotstein@gmx.de>
2016-07-10 21:54:12 +0200

    Initial commit

---
+++ Kinect-HumanAPI/Core/Body/BodyEventArgs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKotstein.Kinect.API.Core
+{
+    public class BodyEventArgs : EventArgs
+    {
+        private readonly IBodyController bc;
+        private readonly ulong trackingId;
+
+        public BodyEventArgs(IBodyController bc, ulong trackingId)
+        {
+            this.bc = bc;
+            this.trackingId = trackingId;
+        }
+
+        public ulong GetTrackingId()
+        {
+            return this.trackingId;
+        }
+
+        public IBodyController GetBodyController()
+        {
+            return this.bc;
+        }
+
+    }
+}
